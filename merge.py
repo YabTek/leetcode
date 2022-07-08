b1cb126a907ce988650d58ef59de30dec6032471
YabTek <yabtekuam@gmail.com>
2022-07-08 19:32:04 +0300

    merge

---
+++ merge.py
@@ -0,0 +1,12 @@
+class Solution:
+    def merge(self, intervals: list[list[int]]) -> list[list[int]]:
+        intervals.sort()
+        merged = []
+        for i in intervals:
+            if len(merged) == 0 or merged[-1][1] < i[0]:
+                merged.append(i)
+            else:
+                merged[-1][1] = max(merged[-1][1], i[1])
+
+        return merged
+        
