f65cda57833d164c1ce0174a27e7bcb90acfca87
YabTek <yabtekuam@gmail.com>
2022-07-09 09:40:47 +0300

    freq

---
+++ frequency.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxFrequency(self, nums: list[int], k: int) -> int:
+        size = len(nums)
+        nums.sort()
+        frequency = 1
+        temp = 1
+        hold1 = 0
+        hold2 = 0
+        while size > temp:
+            hold1 += (nums[temp] - nums[temp - 1]) * (temp - hold2)
+            temp += 1
+            while hold1 > k:
+                hold1 -= nums[temp - 1] - nums[hold2]
+                hold2 += 1
+            frequency = max(frequency, temp - hold2)
+        return frequency
+        
+        
