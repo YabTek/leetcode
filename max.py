0460723ac9ef8aaa003c8b951fe1d2ec902918a3
YabTek <yabtekuam@gmail.com>
2022-08-31 13:37:11 +0300

    sum

---
+++ max.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxOperations(self, nums: list[int], k: int) -> int:
+        
+        nums.sort()
+        operation = 0
+        to_right = 0
+        to_left = len(nums)-1
+      
+        while to_right < to_left:
+            if nums[to_right] +nums[to_left]<k:
+
+                to_right+=1
+            elif nums[to_right] +nums[to_left]==k:
+                operation +=1
+                to_right+=1
+                to_left-=1
+         
+          
+            else:
+                to_left-=1
+        return operation
