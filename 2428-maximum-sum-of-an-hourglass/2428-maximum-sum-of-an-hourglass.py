19955b24562e62599786b26225f49961af5bdc70
YabTek <90649406+YabTek@users.noreply.github.com>
2022-12-17 10:59:33 +0300

    Time: 287 ms (90.53%), Space: 17.1 MB (49.17%) - LeetHub

--- 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
@@ -7,22 +7,16 @@ class Solution:
         
         for i in range(row-1):
             for j in range(col-1):
-                center = Solution.center(grid,i,j)
-                top = Solution.topSum(grid,i,j)
-                bottom = Solution.bottomSum(grid,i,j)
+                center = grid[i+1][j+1]
+                top_sum =  grid[i][j] + grid[i][j+1] + grid[i][j+2]
+                bottom_sum = grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2]
                 
-                pre_sum =  center + top + bottom
+                pre_sum =  center + top_sum + bottom_sum
                 ans = max(ans,pre_sum)
+                
         return ans 
 
-    def topSum(grid,i,j):
-            return grid[i][j] + grid[i][j+1] + grid[i][j+2]
-        
-    def bottomSum(grid,i,j):
-            return grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2]
-        
-    def center(grid,i,j):
-            return grid[i+1][j+1]
+    
        
 				
         
