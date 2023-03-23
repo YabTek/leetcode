f5a874ea80d9ad73eea043b41a8f3ad0c473f925
YabTek <90649406+YabTek@users.noreply.github.com>
2023-03-23 22:41:38 +0300

    Time: 37 ms (41.62%), Space: 13.8 MB (46.83%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        
+        if n <= 0:
+            return False
+        
+        for divisor in [2,3,5]:
+            while n % divisor == 0:
+                n //= divisor
+                
+        return n == 1
