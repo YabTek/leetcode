1c23ba23a9649df8b09711185a2bd5e015467b6e
YabTek <90649406+YabTek@users.noreply.github.com>
2023-03-30 08:00:59 +0300

    Time: 46 ms (31.07%), Space: 14.1 MB (49.63%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,18 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def backtrack(arr,ans):
+        def backtrack(start,ans):
             if len(ans) == len(nums):
                 final.append(ans.copy())
                 return
                
-            for i in range(len(arr)):
-                ans.append(arr[i])
-                new_arr = arr[:i] + arr[i+1:]
-                backtrack(new_arr,ans)
-                ans.pop()
+            for i in range(len(nums)):
+                if (1 << i) & start == 0:
+                    ans.append(nums[i])
+                    backtrack(start | 1 << i,ans)
+                    ans.pop()
                 
         final = []
-        backtrack(nums,[])
+        backtrack(0,[])
         return final
     
     
