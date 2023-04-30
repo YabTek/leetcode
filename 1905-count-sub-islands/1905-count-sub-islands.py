19ab25d79be96bec48c07c5e6bd2cd684fddf8e2
YabTek <90649406+YabTek@users.noreply.github.com>
2023-04-30 00:47:07 -0700

    Time: 3292 ms (34.30%), Space: 105.9 MB (31.60%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,32 +1,36 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-    
-        n = len(grid2)
-        m = len(grid2[0])
+        n = len(grid1)
+        m = len(grid1[0])
+        
+        visited = set()
+        ans = 0
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
         
         def inBound(i,j):
             if 0 <= i < n and 0 <= j < m:
                 return True
-        def dfs(i, j):
-            temp = grid1[i][j] == 1
+            
+        def dfs(i,j):
+            flag = grid1[i][j] == 1
             visited.add((i,j))
-
-            for a, b in directions:
-                new_row = a + i
-                new_col = b + j
+            
+            for dr,dc in directions:
+                r = dr + i
+                c = dc + j
                 
-                if inBound(new_row,new_col) and grid2[new_row][new_col] == 1 and (new_row,new_col) not in visited:
-                    temp = dfs(new_row,new_col) and temp
-            return temp
+                if inBound(r,c) and(r,c) not in visited and grid2[r][c] == 1:
+                    flag = flag & dfs(r,c)
+                    
+            return flag
+                
         
-        directions = [(-1,0),(1,0),(0,1),(0,-1)]
-        visited = set()
-        ans = 0
-
         for i in range(n):
             for j in range(m):
-                if (i,j) not in visited and grid2[i][j] == 1:
-                    if dfs(i,j):
-                        ans += 1
+                if (i,j) not in visited and grid2[i][j] == 1 and dfs(i,j):
+                    ans += 1
+                
         return ans
         
+    
+       
