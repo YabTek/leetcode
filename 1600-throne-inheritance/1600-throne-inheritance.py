3715bbf3279fd02f0375115f6138b8e6959d3e21
YabTek <90649406+YabTek@users.noreply.github.com>
2023-04-30 23:42:18 -0700

    Time: 789 ms (63.04%), Space: 72.3 MB (32.61%) - LeetHub

---
+++ 1600-throne-inheritance/1600-throne-inheritance.py
@@ -0,0 +1,33 @@
+class ThroneInheritance:
+
+    def __init__(self, kingName: str):
+        self.graph = defaultdict(list)
+        self.kingName = kingName
+        self.visited = set()
+    def birth(self, parentName: str, childName: str) -> None:
+        self.graph[parentName].append(childName)
+
+    def death(self, name: str) -> None:
+        self.visited.add(name)
+        
+    def getInheritanceOrder(self) -> List[str]:
+        ans = []
+        
+        def dfs(node):
+            if node not in self.visited:
+                ans.append(node)
+            for child in self.graph[node]:
+                dfs(child)
+                
+        dfs(self.kingName)
+        return ans
+                
+        
+        
+
+
+# Your ThroneInheritance object will be instantiated and called as such:
+# obj = ThroneInheritance(kingName)
+# obj.birth(parentName,childName)
+# obj.death(name)
+# param_3 = obj.getInheritanceOrder()
