c73305b1d22bfb863d320531d2595d4cec05947c
YabTek <90649406+YabTek@users.noreply.github.com>
2023-05-04 08:24:35 -0700

    Time: 2304 ms (53.12%), Space: 199.5 MB (29.11%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.py
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.py
@@ -2,22 +2,23 @@ class Solution:
     def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]: 
        
         def dfs(node):
-           
+            visited.add(node)
+            
             counter = defaultdict(int)
             counter[labels[node]] += 1
 
             for neighbour in graph[node]:
-                graph[neighbour].remove(node)
-
-                for key, val in dfs(neighbour).items():
-                    counter[key] += val
+                if neighbour not in visited:
+                    for letter,count in dfs(neighbour).items():
+                        counter[letter] += count
 
             ans[node] = counter[labels[node]]
-            
+
             return counter
         
         graph = defaultdict(list)
         ans = [1]*n
+        visited = set()
 
         for a, b in edges:
             graph[a].append(b)
