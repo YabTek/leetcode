6b60da41c7c6c8080367d5bd37fe7c62577bf602
YabTek <90649406+YabTek@users.noreply.github.com>
2023-07-28 01:07:41 -0700

    Time: 220 ms (82.72%), Space: 44.4 MB (6.16%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,17 +1,14 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        n = len(nums)
         
         @cache
-        def dp(idx,tot):
-            
-            if idx == n and tot == target:
-                return 1
-            elif idx >= n:
+        def dp(idx,tot): 
+            if idx == len(nums):
+                if tot == target:
+                    return 1
                 return 0
-              
-            return dp(idx+1,tot+nums[idx]) + dp(idx+1,tot-nums[idx])
-
-
+            return dp(idx + 1, tot + nums[idx]) + dp(idx + 1, tot - nums[idx])      
+        
         return dp(0,0)
+        
         
