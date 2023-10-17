78bfa0c252f0c3d94a9ac248963cf9a1db2ecb22
YabTek <90649406+YabTek@users.noreply.github.com>
2023-10-16 23:54:44 -0700

    Time: 124 ms (58.28%), Space: 19.4 MB (64.40%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        n = len(s)
+        answer = 0
+        stk = []
+        sign = "+"
+        number = 0
+        for i in range(n):
+            if s[i].isnumeric():
+                number = number*10 + int(s[i])
+            if s[i] in "+-*/" or i == n-1:
+                if sign == "+":
+                    stk.append(number)
+                elif sign == "-":
+                    stk.append(-number)
+                elif sign == "*":
+                    temp = stk.pop()*number
+                    stk.append(temp)
+                elif sign == "/":
+                    temp = stk.pop()/number
+                    stk.append(int(temp))
+                number = 0
+                sign = s[i]
+        for results in stk:
+            answer += results
+        
+        return answer
+        
+
+                    
