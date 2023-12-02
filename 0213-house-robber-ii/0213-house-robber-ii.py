e17b4cb5387a1dcd6863488a26b4f2aaa8531c88
YabTek <90649406+YabTek@users.noreply.github.com>
2023-12-02 02:14:26 -0800

    Time: 34 ms (88.45%), Space: 16.4 MB (5.70%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,28 +1,20 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        
-        if n == 1:
+        if len(nums) == 1:
             return nums[0]
         
-        dict_right = {}
-        def dp1(i):
-            if i <= 0:
-                return 0
-            if i not in dict_right:
-                dict_right[i] = max(nums[i]+dp1(i-2),dp1(i-1))
+        def rob(nums):
+            n = len(nums)
+            if n == 1:
+                return nums[0]
             
-            return dict_right[i]
-        
-        dict_left = {}
-        def dp2(i):
-            if i >= n-1:
-                return 0
-            if i not in dict_left:
-                dict_left[i] = max(nums[i]+dp2(i+2),dp2(i+1))
-                
-            return dict_left[i]
-            
-        
-        return max(dp1(n-1),dp2(0))
+            dp = [0] * n
+            dp[0] = nums[0]
+            dp[1] = max(nums[0],nums[1])
+
+            for i in range(2, n):
+                dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
+            return dp[-1]
+        return max(rob(nums[1:]), rob(nums[:-1]))
         
