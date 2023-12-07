b404e43c545a35e45d5a5e4cab019bbc0a17200a
YabTek <90649406+YabTek@users.noreply.github.com>
2023-12-06 17:54:10 -0800

    Time: 51 ms (70.31%), Space: 16.4 MB (63.02%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
@@ -1,41 +1,40 @@
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
-        ans = True
-        n = len(equations)
         parent = {}
-        
         for i in range(26):
-            char = chr(i + 97)
+            char = chr(i+97)
             parent[char] = char
-        
+            
         def find(x):
-            if x != parent[x]:
-                parent[x] = find(parent[x])
+            if x == parent[x]:
+                return x
+            parent[x] = find(parent[x])
             return parent[x]
         
         def union(x,y):
             xrep = find(x)
             yrep = find(y)
             
-            if xrep == yrep:
-                return
-            parent[yrep] = xrep
-            
-        def isConnected(x,y):
-            return find(x) == find(y)
-        
-        for i in range(n):
-            if equations[i][1] == "=":
-                union(equations[i][0],equations[i][3])
+            if xrep != yrep:
+                if xrep < yrep:
+                    parent[yrep] = xrep
+                else:
+                    parent[xrep] = yrep
                 
-        for i in range(n):
-            char1 = equations[i][0]
-            char2 = equations[i][1]
-            char3 = equations[i][3]
-            
-            if equations[i][1] == "!" and isConnected(char1,char3):
+          
+        for a,b,c,d in equations:
+            if b == "=":
+                union(a,d)
+         
+        ans = True
+        for a,b,c,d in equations:
+            if b == "!" and find(a) == find(d):
                 ans = False
                 
-        return ans      
             
+        return ans
+                
             
+                
+            
+        
