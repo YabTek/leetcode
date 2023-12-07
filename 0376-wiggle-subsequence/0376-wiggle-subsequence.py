a611a046b132fdadb100ee57b9396c81e42d0d96
YabTek <90649406+YabTek@users.noreply.github.com>
2023-12-07 08:29:03 -0800

    Time: 36 ms (87.33%), Space: 16.4 MB (38.32%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.py
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.py
@@ -3,19 +3,20 @@ class Solution:
         n = len(nums)
         if n < 2:
             return n
-
-        d = {}
-
-        def dp(i, up):
-            if (i,up) in d:
-                return d[(i,up)]
-
-            max_length = 1
-            for j in range(i + 1, n):
-                if (up and nums[j] < nums[i]) or (not up and nums[j] > nums[i]):
-                    max_length = max(max_length, 1 + dp(j, not up))
-
-            d[(i,up)] = max_length
-            return max_length
-
-        return max(dp(0, True), dp(0, False))
+        
+        up = [1]*n
+        down = [1]*n
+        
+        for i in range(1,n):
+            if nums[i] > nums[i-1]:
+                up[i] = down[i-1] + 1
+                down[i] = down[i-1]
+            elif nums[i] < nums[i-1]:
+                down[i] = up[i-1] + 1
+                up[i] = up[i-1]
+            else:
+                up[i] = up[i-1]
+                down[i] = down[i-1]
+                
+        return max(up[-1], down[-1])
+        
