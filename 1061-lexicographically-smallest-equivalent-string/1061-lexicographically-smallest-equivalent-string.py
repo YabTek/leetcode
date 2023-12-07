fa688f59bf19a15c18616c862b69e7f5e9333531
YabTek <90649406+YabTek@users.noreply.github.com>
2023-12-06 17:05:05 -0800

    Time: 47 ms (52.56%), Space: 16.4 MB (22.16%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -1,40 +1,34 @@
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-    
-        def find(x):
-            
-            if x != parent[x]:
-                parent[x]  = find(parent[x])
-            return parent[x]
-            
-        def union(x,y):
-            xrep = find(x)
-            yrep = find(y)
-            
-            if xrep == yrep:
-                return 
-            elif xrep < yrep:
-                parent[yrep] = xrep
-            else:
-                parent[xrep] = yrep
-           
-            
-        ans = ""  
         parent = {}
         
         for i in range(26):
             char = chr(i+97)
             parent[char] = char
-             
+              
+        
+        def find(x):
+            while x != parent[x]:
+                x = parent[x]
+            return x
+        def union(x,y):
+            xrep = find(x)
+            yrep = find(y)
+            
+            if xrep != yrep:
+                if xrep < yrep:
+                    parent[yrep] = xrep
+                else:
+                    parent[xrep] = yrep
+                    
+                    
         for a,b in zip(s1,s2):
-            if a == b:
-                continue
             union(a,b)
             
+        ans = []
         for char in baseStr:
-            ans += find(char)
+            ans.append(find(char))
             
-        return ans
-        
-        
+        return "".join(ans)
+                    
         
