e1f968f423557dc7aee7b1899d1290556894e337
YabTek <90649406+YabTek@users.noreply.github.com>
2023-12-07 11:13:53 -0800

    Time: 2146 ms (72.80%), Space: 34 MB (66.96%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.py
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.py
@@ -3,11 +3,16 @@ class Solution:
         n = len(nums)
         dp = [{} for _ in range(n)]
         ans = 0
-
+        
         for i in range(n):
             for j in range(i):
                 diff = nums[i] - nums[j]
-                dp[i][diff] = dp[j][diff] + 1 if diff in dp[j] else 1 + 1
-                ans = max(ans, dp[i][diff])
-
+                if diff in dp[j]:
+                    dp[i][diff] = dp[j][diff] + 1
+                else:
+                    dp[i][diff] = 2
+                    
+                ans = max(ans,dp[i][diff])
+                
         return ans
+        
