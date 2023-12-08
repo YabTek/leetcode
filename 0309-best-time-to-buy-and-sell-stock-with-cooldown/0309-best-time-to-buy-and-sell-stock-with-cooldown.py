bb768a9e5ba7770b496ff6854259fe6d3915d94a
YabTek <90649406+YabTek@users.noreply.github.com>
2023-12-07 23:06:35 -0800

    Time: 46 ms (75.73%), Space: 16.7 MB (65.17%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,23 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        
+        if not prices:
+            return 0
+
         n = len(prices)
-        
-        @cache
-        def dp(i,cur):
-            if i >= n:
-                return 0
+        buy = [0] * n
+        sell = [0] * n
+        cooldown = [0] * n
+
+        buy[0] = -prices[0]
+
+        for i in range(1, n):
+            cooldown[i] = max(cooldown[i - 1], sell[i - 1])
+            buy[i] = max(buy[i - 1], cooldown[i - 1] - prices[i])
+            sell[i] = buy[i - 1] + prices[i]
+
+        return max(sell[-1], cooldown[-1])
             
-            if cur == "buy":
-                return max(dp(i+1,"sell") - prices[i], dp(i+1,"buy"))
-            
-            if cur == "sell":
-                return max(dp(i+1,"cooldown") + prices[i], dp(i+1,"sell"))
-            
-            if cur == "cooldown":
-                return dp(i+1,"buy")
-            
-        return dp(0,"buy")
-            
-       
-        
-        
