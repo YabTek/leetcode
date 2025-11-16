203818921d25805b9537dfe1ad620dbc4f2500df
YabTek <yeabsira.tekuamwork@a2sv.org>
2025-11-16 16:42:25 +0100

    leetcode

---
+++ 1513. Number of Substrings With Only 1s.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numSub(self, s: str) -> int:
+        ans = 0
+        count = 0
+
+        for i in range(len(s)):
+            if s[i] == "1":
+                count += 1
+                ans += count
+            else:
+                count = 0
+
+        return ans % (10**9 + 7)
+
+
+
+        
