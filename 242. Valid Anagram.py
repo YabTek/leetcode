df7aa4c4b9adafb490237c37314e0ed42f6d7d64
YabTek <yeabsira.tekuamwork@a2sv.org>
2025-12-04 14:05:14 +0100

    leetcode

---
+++ 242. Valid Anagram.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        d = defaultdict(int)
+
+        for i in range(len(s)):
+            d[s[i]] += 1
+            d[t[i]] -= 1
+
+        for val in d.values():
+            if val != 0:
+                return False
+
+        return True
+        
